c0dd51846bcae124895351d9c7fc048024416098
seowonkim <ashley0kimmie@gmail.com>
2017-10-10 15:32:04 -0400

    Oct10

---
+++ creative-computing/javascript/javascript.js
@@ -0,0 +1,13 @@
+var animals = [
+	"cat",
+	"dog",
+	"pig",
+	"toucan"
+	"turkey"
+	"fish"
+]
+
+for (var i=0; i<animals.length;i=i+1) {
+	var animal = animals[i];
+	console.log ("<img src='" + animal + “.jpg’ />)
+}
