f12c40c9c359ca5a9cdae51eb550329d3413eab1
seowonkim <Kims923@newschool.edu>
2018-04-16 11:32:17 -0400

    push

---
+++ 8_js_exercise/index.js
@@ -0,0 +1,36 @@
+var myWeekend = 'cooking'
+
+6*11 
+60 + 6 
+'6' + '6'
+660/10 
+70-4 
+
+var one = [
+'Emil',
+'Kristen',
+'Emily',
+'Evelin',
+'Esra',
+'Prithi'
+]
+
+var two = [
+"Khe"
+"Vincent"
+"Vinny"
+"Shuting"]
+
+var three = [
+"Soyeon"
+"Seowon"
+"Jahyun"]
+
+var four = [
+"Yaren"
+"Yangchen"]
+
+var allStudentsHere = [
+]
+
+var studentsMissing ['no one is absent']
